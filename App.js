de9976a56da6eea736b75da82eaaa38a3bc0e0e5
augusto-mattos <guto.s.mattos@gmail.com>
2024-04-26 14:30:11 +0200

    firebase config

--- App.js
+++ App.js
@@ -1,7 +1,19 @@
+import { initializeApp } from "firebase/app";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Landing from "./components/auth/Landing";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCxeK9XXQUNiCZCz80sw9IhniN5eWZ7fjs",
+  authDomain: "imagigram-c345f.firebaseapp.com",
+  projectId: "imagigram-c345f",
+  storageBucket: "imagigram-c345f.appspot.com",
+  messagingSenderId: "1056126235957",
+  appId: "1:1056126235957:web:6a890aee62cb319bee487c"
+};
+
+initializeApp(firebaseConfig);
 
 const Stack = createNativeStackNavigator();
 
